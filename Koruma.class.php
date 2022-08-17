0ab82b366fc709c743c66f0717181b84c3588db6
Rodeiun <78654486+Rodeiun@users.noreply.github.com>
2022-08-17 07:35:16 +0300

    Main Files
    Ana Dosyalar

---
+++ Koruma.class.php
@@ -0,0 +1,41 @@
+<?php
+    namespace Main;
+    class Koruma 
+    {
+        public static function QueryString($str)
+        {
+            $yeni = trim($str);
+            $yeni = strip_tags($yeni);
+            $yeni = str_replace(["insert", "INSERT", "delete", "DELETE", "select", "SELECT", "exec", "EXEC", "union", "UNION", "drop", "DROP"], "", $yeni);
+            return $yeni;
+        }
+        public static function Input($str)
+        {
+            $yeni = trim($str);
+            $yeni = strip_tags($yeni);
+            $yeni = htmlspecialchars($yeni, ENT_QUOTES);
+            return $yeni;
+        }
+        public static function Exists($value)
+        {
+            switch($value)
+            {
+                case 'POST':
+                    {
+                        return (!empty($_SERVER["REQUEST_METHOD"] == "POST")) ? true : false;
+                        break;
+                    }
+                case 'GET':
+                    {
+                        return (!empty($_GET)) ? true : false;
+                        break;
+                    }
+                default:
+                    {
+                        return false;
+                        break;
+                    }
+            }
+        }
+    }
+?>
